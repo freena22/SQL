e90bf6e08a40d886a5e64eeaef90cf14d4dc4095
freena22 <freena22@gmail.com>
2018-01-23 19:30:16 -0800

    group by and having

--- Basic_SQL_Practice_1.sql
+++ Basic_SQL_Practice_1.sql
@@ -38,3 +38,37 @@ FROM recent_grads ORDER BY quartile_spread LIMIT 20;
 
 ## <Group Summary Statistics>
 
+# Group By
+
+SELECT Major_category, AVG(Employed) / AVG(Total) share_employed
+FROM recent_grads
+GROUP BY Major_category;
+
+# Having and Where
+# The HAVING clause was added to SQL because the WHERE keyword could not be used with aggregate functions. 
+# HAVING clause is like a WHERE clause, but applies only to groups as a whole 
+# that is, to the rows in the result set representing groups, whereas the WHERE clause applies to individual rows
+
+WHERE comes before GROUP BY; SQL evaluates the WHERE clause before it groups records.
+SELECT Major_category, AVG(Low_wage_jobs) / AVG(Total) AS share_low_wage FROM recent_grads
+GROUP BY Major_category
+HAVING share_low_wage > 0.1;
+
+# Round
+SELECT ROUND(ShareWomen, 2), Major_category
+FROM recent_grads
+LIMIT 10;
+
+# Nesting Function
+SELECT Major_category, ROUND(AVG(College_jobs) / AVG(Total), 3) AS share_degree_jobs
+FROM recent_grads
+GROUP BY Major_category
+HAVING share_degree_jobs < 0.3;
+
+# Casting -- transfer the interger to float
+SELECT CAST(Women as Float) / CAST(Total as Float) FROM recent_grads limit 5
+
+## <Subqueries>
+
+
+
